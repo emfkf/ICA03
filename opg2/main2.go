2b0ffac6fbdd0370608ce61574ee2d463a37d88f
Fredrik Meltveit <emkf.meltveitf@gmail.com>
2019-03-20 12:26:18 +0100

    Add terminal flag

--- opg2/main2.go
+++ opg2/main2.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"flag"
 	"ica03/opg2/fileinfo"
 )
 
 var file1, file2, file3 = "../files/text1.txt", "../files/text2.txt", "../files/pg100.txt"
 
 func main() {
-	fileinfo.PrintFileInfo(file1)
+	f := flag.String("f", file1, "file to be used")
+	fileinfo.PrintFileInfo(*f)
 }
